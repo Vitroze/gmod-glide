1283de074a69dfcc959c9fcb484676c6a7aadd50
Vitroze <azouaouiaziz4@gmail.com>
2026-09-02 02:46:22 +0200

    Fix for the convar name

--- lua/autorun/sh_glide.lua
+++ lua/autorun/sh_glide.lua
@@ -200,7 +200,7 @@ CreateConVar( "glide_pacifist_mode", "0", FCVAR_ARCHIVE + FCVAR_NOTIFY + FCVAR_R
 CreateConVar( "glide_allow_gravity_gun_punt", "0", FCVAR_ARCHIVE + FCVAR_NOTIFY + FCVAR_REPLICATED, "When set to 1, allows players to push vehicles with the Gravity Gun.", 0, 1 )
 CreateConVar( "glide_enable_damage_player_on_collision", "0", FCVAR_ARCHIVE + FCVAR_REPLICATED, "Whether players inside vehicles can take damage from hard collisions.", 0, 1 )
 CreateConVar( "glide_always_can_enter_locked_vehicles", "0", FCVAR_ARCHIVE + FCVAR_REPLICATED, "Whether players can enter locked vehicles they don't own.", 0, 1 )
-CreateConVar( "glide_allow_take_weapon_in_vehicle", "0", FCVAR_ARCHIVE + FCVAR_REPLICATED, "Allows player to use their weapons in a vehicle.", 0, 1 )
+CreateConVar( "glide_allow_weapons_in_vehicle", "0", FCVAR_ARCHIVE + FCVAR_REPLICATED, "Allows player to use their weapons in a vehicle.", 0, 1 )
 
 -- Sandbox limits
 cleanup.Register( "glide_vehicles" )
